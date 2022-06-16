b2fa70b9c1ac6ea613bfe734a85b169f6939b6f0
Farah <104321986+FarahXM@users.noreply.github.com>
2022-06-16 18:44:10 +0100

    Add files via upload

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -9,7 +9,8 @@
 ---------- base -----------
 import XMonad
 import Control.Monad (liftM2)
-import qualified XMonad.StackSet as W
+import qualified XMonad.StackSet as W   -- for Floating Window 
+import qualified Data.Map as M          -- for Floating Window
 
 -------- Actions ---------
 import XMonad.Actions.UpdatePointer
@@ -90,15 +91,14 @@ white_       = "#acb0d0"
 
 --------------------------------------------- Variable XMonad --------------------------------------------
 myModMask       = mod1Mask                                                          -- Sets Mod Key to alt/Super/Win/Fn.
-myTerminal      = "alacritty"                                                       -- Sets default Terminal Emulator.
+myTerminal      = "kitty"                                                           -- Sets default Terminal Emulator.
 myBorderWidth   = 0                                                                 -- Sets Border Width in pixels.
 myNormalColor   = bg                                                                -- Border color of normal windows.
 myFocusedColor  = blue                                                              -- Border color of focused windows.
 myFont          = "xft:JetBrains Mono:Regular:size=10"
-myBigFont       = "xft:Source Code Pro:Bold:size=50"
+myBigFont       = "xft:FiraCode Nerd Font Mono:Bold:size=50"
 myWideFont      = "xft:DejaVu Sans Mono:Bold:size=90:antialias=true:hinting=true"
 windowCount     = gets $ Just . show . length . W.integrate' . W.stack . W.workspace . W.current . windowset  -- Number Of Window in XMobar
-
 
 ------ Workspaces -------
 -- wsDEV           = "¹DEV"
@@ -119,14 +119,14 @@ myStartupHook = do
     spawnOnce "unclutter"                                                           -- hidden Mouse
     spawnOnce "nitrogen --restore"                                                  -- feh is the alternative "feh --bg-scale /directory/of/desired/background &"
     spawnOnce "xset r rate 300 55"                                                  -- speeds cursor in urxvt
-    spawnOnce "picom --experimental-backends"                                       -- Compositor
+    spawnOnce "picom --experimental-backends -b"                                    -- Compositor
     setDefaultCursor xC_left_ptr                                                    -- Default Cursor
 
 ------------------------------------------ Layout --------------------------------------------------------
 mySpacings       = spacingRaw False (Border 0 0 0 0) True (Border 5 5 5 5) True
 myGaps           = gaps [(U, 10),(D, 5),(L, 10),(R, 10)]
 myShowWNameTheme = def
-                { swn_font              = myWideFont
+                { swn_font              = myBigFont
                 , swn_fade              = 1.0
                 , swn_bgcolor           = bg
                 , swn_color             = blue
@@ -134,21 +134,21 @@ myShowWNameTheme = def
 
 myTopBar = noFrillsDeco shrinkText myTopBarTheme
 myTopBarTheme = def 
-               { fontName      = myFont
+               { fontName           = myFont
               , activeColor         = blue
               , activeBorderColor   = blue
-              , activeBorderWidth   = 20
+              , activeBorderWidth   = 15
               , activeTextColor     = bg
               , inactiveColor       = black
               , inactiveBorderColor = black
-              , inactiveBorderWidth = 20
+              , inactiveBorderWidth = 15
               , inactiveTextColor   = bg
               , urgentColor         = red
               , urgentBorderColor   = red
-              , urgentBorderWidth   = 20
+              , urgentBorderWidth   = 15
               , urgentTextColor     = red
-              , decoWidth           = 20
-              , decoHeight          = 20
+              , decoWidth           = 15
+              , decoHeight          = 15
               }
 
 -------------------- Base Layout ------------------------
@@ -247,53 +247,50 @@ full            = renamed [Replace "FULL"]
 masterTabbed    = renamed [Replace "MASTER TABBED"]       
                 $ maximizeWithPadding 16 
                 $ minimize 
-                $ mySpacings 
                 $ mastered (1/100) (1/2) $ tabbed shrinkText myTabTheme
 
 oneUp           = renamed [Replace "1UP"]
                 $ myTopBar
-                $ myGaps
-                $ mySpacings
+                $ mySpacings 
                 $ combineTwoP (ThreeCol 1 (3/100) (1/2))
                                     (Simplest)
                                     (Tall 1 0.03 0.5)
-                                    (ClassName "firefox")
+                                    (ClassName "mpv")
 
 twoTabbed       = renamed [Replace "TWO TABBED"]
-                $ mySpacings
                 $ combineTwoP (TwoPane 0.03 0.5) 
                               (tabbed shrinkText myTabTheme) 
                               (tabbed shrinkText myTabTheme) 
-                              (ClassName "firefox")
+                              (ClassName "mpv")
 
 ---------------------------- Usage ------------------------------
-allLayouts  = tall ||| full ||| twoPane ||| threeColMid ||| oneBig ||| dishes ||| circle ||| grid  ||| spirals ||| masterTabbed ||| oneUp ||| twoTabbed
-webLayouts  = oneBig ||| threeColMid ||| dishes ||| tall ||| twoTabbed ||| oneUp 
-codeLayouts = dishes ||| twoPane ||| tabs 
-chatLayouts = grid ||| threeColMid ||| tall
-youtubeLayouts  = oneBig ||| full
-settingsLayouts = circle ||| grid ||| spirals ||| floats
-mediaLayout = circle ||| spirals ||| floats
-gameLayout  = masterTabbed |||floats
-
 myLayoutHook    = showWName' myShowWNameTheme
                 $ mkToggle (NOBORDERS ?? FULL ?? EOT)
                 $ limitWindows 12
                 $ avoidStruts
-                $ onWorkspace " 1 " codeLayouts
-                $ onWorkspace " 2 " mediaLayout
-                $ onWorkspace " 9 " gameLayout
+                $ onWorkspaces [" 1 "," 2 "] codeLayouts
                 $ onWorkspaces [" 3 "," 4 "] webLayouts
                 $ onWorkspaces [" 5 "," 6 "] chatLayouts
                 $ onWorkspaces [" 7 "," 8 "] settingsLayouts
+                $ onWorkspace " 9 " mediaLayout
                 $ allLayouts
+               where 
+    allLayouts = tall ||| full ||| twoPane ||| threeColMid ||| oneBig ||| dishes ||| grid
+    webLayouts = oneBig ||| threeColMid ||| dishes ||| tall
+      -- ||| floats
+      -- ||| grid
+      -- ||| spirals
+    codeLayouts = tabs ||| twoPane ||| dishes
+    chatLayouts = grid ||| threeColMid ||| tall
+    youtubeLayouts = oneBig ||| full
+    settingsLayouts = circle ||| grid ||| spirals ||| floats
+    mediaLayout = oneUp ||| twoTabbed ||| masterTabbed ||| tabs 
+
 
 --------------------------------------------------  keyBidings --------------------------------------------
 myKeys =            -- Programme --
-         [ ("M-w",          raiseBrowser)
-         , ("M-S-f",        spawn "alacritty -e nnn"                )
-         , ("M-S-m",        spawn "alacritty -e cmus"                )
-         , ("M-d",          spawn "rofi -show drun -Show-icons"     )
+         [ ("M-w",          raiseBrowser                             )
+         , ("M-d",          spawn "rofi -show drun -Show-icons"      )
          , ("M-S-d",        spawn "dmenu_run -fn 'JetBrains Mono:style=Bold:pixelsize=12' -nb '#11121D' -nf '#7aa2f7' -sb '#7aa2f7' -sf '#11121D' -p 'CMD:'")
 
                     -- ScreenShoot --
@@ -302,34 +299,36 @@ myKeys =            -- Programme --
          , ("M-<Print>",    spawn "scrot -u -F ~/pix/screen/%Y-%m-%d-%T-screenshot.png && notify-send -t 800 'ScreenShot Takeen' 'Saved in ~/pix/screen/'"  )
 
                     -- Audio ---
-         , ("C-<F8>",         spawn "pactl set-sink-volume @DEFAULT_SINK@ -10% && notify-send -t 200 `pulsemixer --get-volume | awk '{print $1}'`" )
-         , ("C-<F9>",         spawn "pactl set-sink-volume @DEFAULT_SINK@ +10% && notify-send -t 200 `pulsemixer --get-volume | awk '{print $1}'`" )
-         , ("C-<F10>",        spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle  && notify-send -t 200 'Toggle mute Mic button'"     )
-         , ("C-<F11>",        spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle && notify-send -t 200 'Toggle mute button!'"             )
+         , ("<F8>",         spawn "pactl set-sink-volume @DEFAULT_SINK@ -10% && notify-send -t 200 `pulsemixer --get-volume | awk '{print $1}'`" )
+         , ("<F9>",         spawn "pactl set-sink-volume @DEFAULT_SINK@ +10% && notify-send -t 200 `pulsemixer --get-volume | awk '{print $1}'`" )
+         , ("<F10>",        spawn "pactl set-source-mute @DEFAULT_SOURCE@ toggle  && notify-send -t 200 'Toggle mute Mic button'"     )
+         , ("<F11>",        spawn "pactl set-sink-mute @DEFAULT_SINK@ toggle && notify-send -t 200 'Toggle mute button!'"             )
 
                     -- Brightenss --
-        , ("C-<F4>",          spawn "xbacklight -set 50 && notify-send -t 200 `xbacklight -get`")
-        , ("C-<F5>",          spawn "xbacklight -dec 10 && notify-send -t 200 `xbacklight -get`")
-        , ("C-<F6>",          spawn "xbacklight -inc 10 && notify-send -t 200 `xbacklight -get`")
-        , ("C-<F7>",          spawn "xbacklight -set 100 && notify-send -t 200 `xbacklight -get`")
+        , ("<F4>",          spawn "xbacklight -set 50 && notify-send -t 200 `xbacklight -get`")
+        , ("<F5>",          spawn "xbacklight -dec 10 && notify-send -t 200 `xbacklight -get`")
+        , ("<F6>",          spawn "xbacklight -inc 10 && notify-send -t 200 `xbacklight -get`")
+        , ("<F7>",          spawn "xbacklight -set 100 && notify-send -t 200 `xbacklight -get`")
 
                     -- Scripts --
-         , ("C-S-w",          spawn "bash ~/.scripts/rofi/wifiMenu.sh" )
-         , ("C-S-0",          spawn "bash ~/.scripts/rofi/powerMenu.sh")
+         , ("C-w",          spawn "bash ~/.scripts/rofi/wifiMenu.sh" )
+         , ("C-0",          spawn "bash ~/.scripts/rofi/powerMenu.sh")
                
 				    -- Scratchpads --
          , ("M-s t",        namedScratchpadAction myScratchPads "terminal")  
          , ("M-s s",        namedScratchpadAction myScratchPads "cmus")        
+         , ("M-s w",        namedScratchpadAction myScratchPads "browser")        
             
                             -- cmus --
-         , ("C-S-z",         spawn "cmus-remote -r")
-         , ("C-S-x",         spawn "cmus-remote -p")
-         , ("C-S-c",         spawn "cmus-remote -u")
-         , ("C-S-v",         spawn "cmus-remote -s")
-         , ("C-S-b",         spawn "cmus-remote -n")
+         , ("M-C-S-z",         spawn "cmus-remote -r")
+         , ("M-C-S-x",         spawn "cmus-remote -p")
+         , ("M-C-S-c",         spawn "cmus-remote -u")
+         , ("M-C-S-v",         spawn "cmus-remote -s")
+         , ("M-C-S-b",         spawn "cmus-remote -n")
 
 
                     --  Modifiers Layout --
+         , ("M-t", withFocused toggleFloat                          ) {-- Floating window --}
          , ("M-f",          sendMessage $ Toggle FULL               ) {--Full Screen --}
          , ("M-e",          viewEmptyWorkspace                      ) {-- Find Empty Workspaces --}
          , ("M-g",          tagToEmptyWorkspace                     ) {-- Go To workspaces --}
@@ -344,12 +343,18 @@ myKeys =            -- Programme --
          , ("M-C-j",        sendMessage MirrorShrink) {-- For Layout ResizableTile( Tiled ) -}
          , ("M-C-k",        sendMessage MirrorExpand) {-- For Layout ResizableTile( Tiled ) -}
          ]
+        where 
+            toggleFloat w = windows (\s -> if M.member w (W.floating s)
+                            then W.sink w s
+                            else (W.float w (W.RationalRect (1/6) (1/6) (2/3) (2/3)) s))
+
+
 
 --------------------------------------------------- ManageHook --------------------------------------------
 myManageHook = composeAll 
      [ className =? "firefox"           --> doViewShift " 3 "
-     , className =? "Thunar"            --> doViewShift " 2 "
-     , className =? "mpv"               --> doCenterFloat
+     , className =? "Thunar"            --> doViewShift " 9 "
+     , className =? "mpv"               --> doViewShift " 9 "
      , className =? "Sxiv"              --> doCenterFloat
      , className =? "Nitrogen"          --> doCenterFloat
      , className =? "download"          --> doFloat
@@ -362,9 +367,10 @@ myManageHook = composeAll
 -------------------------------------------------- ScratchPads -------------------------------------------
 myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
                 , NS "cmus" spawnCmus findCmus manageCmus
+                , NS "browser" spawnBrowser findBrowser manageBrowser
                 ]
   where
-    spawnTerm  = myTerminal ++ " -t scratchpad"
+    spawnTerm  = myTerminal ++ " -T scratchpad"
     findTerm   = title =? "scratchpad"
     manageTerm = customFloating $ W.RationalRect l t w h
                where
@@ -372,7 +378,7 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
                  w = 0.9
                  t = 0.95 -h
                  l = 0.95 -w
-    spawnCmus   = myTerminal ++ " -t cmus -e cmus"
+    spawnCmus   = myTerminal ++ " -T cmus -e cmus"
     findCmus    = title =? "cmus"
     manageCmus  = customFloating $ W.RationalRect l t w h
                where
@@ -380,7 +386,9 @@ myScratchPads = [ NS "terminal" spawnTerm findTerm manageTerm
                  w = 0.9
                  t = 0.95 -h
                  l = 0.95 -w
-
+    spawnBrowser = "chromium"
+    findBrowser = className =? "Chromium"
+    manageBrowser = customFloating $ W.RationalRect (1/10) (1/20) (4/5) (9/10)
 
 -------------------------------------------------- Aplicy All   -------------------------------------------
 main = do
@@ -425,4 +433,3 @@ main = do
                                                        >>  updatePointer (0.5, 0.5) (0, 0) -- exact centre of window
                                                        >> fadeInactiveLogHook 0.90         -- Trancperncy Window (max  = 1)
                                } `additionalKeysP` myKeys
-
